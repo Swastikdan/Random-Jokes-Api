828d63c6c431dfd9c5783eb5d8faffdc829010ce
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2023-11-12 12:31:50 +0530

    Query by id fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import required modules
 require('dotenv').config();
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const path = require('path');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
@@ -27,7 +27,7 @@ app.use(cors());
 // Set up rate limiting
 const limiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 60 minutes
-    max: 1000, // limit each IP to 1000 requests per windowMs
+    max: 10000, // limit each IP to 1000 requests per windowMs
 });
 app.use(limiter);
 
@@ -38,6 +38,45 @@ app.use(express.static('public'));
 const uri = process.env.MONGODB_URI;
 
 let client;
+
+async function handleJokeRequest(db, collectionName, req, res) {
+    // Parse query parameters
+    let result = parseInt(req.query.result) || 1;
+    let id = req.query.id;
+    let password = req.query.password;
+
+    // Check if result is within allowed limit
+    if (result > MAX_RESULTS && (!password || !PASSWORD.includes(password))) {
+        res.json({ success: false, error: `Parameter value not allowed. Maximum value for "result" is ${MAX_RESULTS}.` });
+        return;
+    }
+
+    try {
+        let docs;
+        // If id is provided, fetch specific document, else fetch random documents
+        if (id) {
+            // Use ObjectId to match by _id field
+            id = new ObjectId(id); // Convert string id to ObjectId
+            docs = await db.collection(collectionName).find({ _id: id }).toArray();
+        } else {
+            docs = await db.collection(collectionName).aggregate([{ $sample: { size: result } }], { allowDiskUse: true }).toArray();
+        }
+
+        // Log the query and response status
+        // if (process.env.NODE_ENV !== 'production') {
+        //     console.log(`Query: ${JSON.stringify(req.query)}`);
+        //     console.log(`Response status: ${docs.length > 0 ? 200 : 404}`);
+        // }
+
+        res.json({ success: true, jokes: docs });
+    } catch (err) {
+        // Log the error
+        // if (process.env.NODE_ENV !== 'production') {
+        //     console.error(err);
+        // }
+        res.json({ success: false, error: 'Error fetching data from the database' });
+    }
+}
 
 (async () => {
     try {
@@ -50,65 +89,20 @@ let client;
 
         // Define '/api/singleliner' route
         app.get('/api/singleliner', async (req, res) => {
-            // Parse query parameters
-            let result = parseInt(req.query.result) || 1;
-            let id = req.query.id;
-            let password = req.query.password;
-
-            // Check if result is within allowed limit
-            if (result > MAX_RESULTS && (!password || !PASSWORD.includes(password))) {
-                res.json({success: false, error: `Parameter value not allowed. Maximum value for "result" is ${MAX_RESULTS}.`});
-                return;
-            }
-
-            try {
-                let docs;
-                // If id is provided, fetch specific document, else fetch random documents
-                if (id) {
-                    docs = await db.collection('Singleliner').find({ id: id }).toArray();
-                } else {
-                    docs = await db.collection('Singleliner').aggregate([{ $sample: { size: result } }]).toArray();
-                }
-                res.json({success: true, jokes: docs});
-            } catch (err) {
-                console.error(err);
-                res.json({ success: false, error: 'Error fetching data from the database' });
-            }
+            handleJokeRequest(db, 'Singleliner', req, res);
         });
 
         // Define '/api/jokes' route
         app.get('/api/jokes', async (req, res) => {
-            // Parse query parameters
-            let result = parseInt(req.query.result) || 1;
-            let id = req.query.id;
-            let password = req.query.password;
-
-            // Check if result is within allowed limit
-            if (result > MAX_RESULTS && (!password || !PASSWORD.includes(password))) {
-                res.json({success: false, error: `Parameter value not allowed. Maximum value for "result" is ${MAX_RESULTS}.`});
-                return;
-            }
-
-            try {
-                let docs;
-                // If id is provided, fetch specific document, else fetch random documents
-                if (id) {
-                    docs = await db.collection('Jokes').find({ id: id }).toArray();
-                } else {
-                    docs = await db.collection('Jokes').aggregate([{ $sample: { size: result } }]).toArray();
-                }
-                res.json({success: true, jokes: docs});
-            } catch (err) {
-                console.error(err);
-                res.json({ success: false, error: 'Error fetching data from the database' });
-            }
+            handleJokeRequest(db, 'Jokes', req, res);
         });
 
         // Define '/api/random' route
         app.get('/api/random', async (_, res) => {
-            // Randomly select a collection
-            const collectionName = Math.random() < 0.5 ? 'Singleliner' : 'Jokes';
             try {
+                // Randomly select a collection
+                const collectionName = Math.random() < 0.5 ? 'Singleliner' : 'Jokes';
+
                 // Fetch a random document from the selected collection
                 const result = await db.collection(collectionName).aggregate([{ $sample: { size: 1 } }]).toArray();
                 res.json({ success: true, joke: result[0] });
@@ -139,3 +133,4 @@ process.on('SIGINT', () => {
     client.close();
     process.exit();
 });
+
